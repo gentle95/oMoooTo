f35f606f23eb5dec916e7c8c5e381d59d59b49b7
gentle95 <loong.zou85@gmail.com>
2025-05-08 17:07:41 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,6 +7,8 @@ chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.nju.edu.cn/docker-ce/linux/debian "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee -a /etc/apt/sources.list.d/docker.list > /dev/null
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
+mkdir /{docker,docker_root}
+chmod -R 777 {docker,docker_root}
 tee /etc/docker/daemon.json <<EOF
 {
   "registry-mirrors": [
