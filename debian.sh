244c3d334d0fbed819357580f99a28ab6ad99556
gentle95 <loong.zou85@gmail.com>
2025-05-12 07:58:15 +0800

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -14,7 +14,7 @@ Signed-By: /usr/share/keyrings/debian-archive-keyring.gpg
 EOF
 
 apt update -y
-apt install -y openssh-server locales curl wget cloud-guest-utils qemu-guest-agent net-tools
+apt install -y openssh-server locales curl wget cloud-guest-utils qemu-guest-agent net-tools cron
 sed -i 's/# zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
 locale-gen
 systemctl enable --now qemu-guest-agent
